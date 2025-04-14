3bda5298f0272e35351a2b9226927c093c5af7f3
Carlo Costa <carlo.costa92@gmail.com>
2025-04-14 14:38:54 +0200

    feat: re create function with alert and reset

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,31 @@ function App() {
   //inserimento endpoint per invio nuovo post
   const endpoint = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts";
 
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    axios
+      .post(endpoint, formData)
+      .then((response) => {
+        console.log("Dati inviati con successo", response.data);
+        alert("Dati inviati con successo");
+
+      })
+      .catch((error) => {
+        console.error("Errore durante invio dei dati:", error);
+        alert("Errore durante invio dati");
+
+      });
+
+    //reset form
+    setFormData({
+      author: '',
+      title: '',
+      body: '',
+      public: true,
+    });
+  }
+
   return (
     <>
       <h1>React Post Form</h1>
