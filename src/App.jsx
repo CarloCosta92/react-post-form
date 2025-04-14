83e8f77f877b30c1b1f6a5279de26a43762db074
Carlo Costa <carlo.costa92@gmail.com>
2025-04-14 14:18:18 +0200

    feat: create html

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,59 @@ function App() {
 
   return (
     <>
+      <h1>React Post Form</h1>
+
+      <div>
+        <label htmlFor="author">inserisci autore </label>
+        <input
+          type="text"
+          name="author"
+          value={formData.author}
+          onChange={handleFormData}
+          placeholder="Inserisci autore"
+
+        />
+
+        <hr />
+      </ div>
+
+      <div>
+        <label htmlFor="title">inserisci titolo </label>
+        <input
+          type="text"
+          name="title"
+          value={formData.title}
+          onChange={handleFormData}
+          placeholder="Inserisci titolo"
+
+        />
+
+        <hr />
+      </ div>
+
+      <div>
+        <label htmlFor="body">inserisci corpo del post </label>
+        <input
+          type="text"
+          name="body"
+          value={formData.body}
+          onChange={handleFormData}
+          placeholder="Inserisci corpo del post"
+
+        />
+
+        <hr />
+      </ div>
+
+      <label>
+        Pubblico:
+        <input
+          type="checkbox"
+          name="public"
+          checked={formData.public}
+          onChange={handleFormData}
+        />
+      </label>
 
     </>
   )
