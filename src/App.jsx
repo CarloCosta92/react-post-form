68cb5f8b8ea5b22f2b573248e607992e75d17fd6
Carlo Costa <carlo.costa92@gmail.com>
2025-04-14 14:04:58 +0200

    feat: created object

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,25 @@ import './App.css'
 
 function App() {
 
+  //variabile di stato per il nostro oggetto da spedire ad api
+  const [formData, setFormData] = useState({
+    author: "",
+    title: "",
+    body: "",
+    public: true
+  });
+
+  //funzione per gestire inserimento
+  function handleFormData(e) {
+    setFormData((formData) => ({
+      ...formData,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+
+
+
 
   return (
     <>
