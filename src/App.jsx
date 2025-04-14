7c63ffaf355fbb78e7392f998a9562ce73901d3f
Carlo Costa <carlo.costa92@gmail.com>
2025-04-14 14:28:11 +0200

    feat: installed axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import axios from 'axios';
 
 import './App.css'
 
@@ -23,6 +24,28 @@ function App() {
 
   //inserimento endpoint per invio nuovo post
   const endpoint = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts";
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    axios
+      .post(endpoint, formData)
+      .then((response) => {
+        console.log('Dati inviati con successo:', response.data);
+
+      })
+      .catch((error) => {
+        console.error('Errore durante l\'invio dei dati:', error);
+
+      });
+
+    setFormData({
+      author: '',
+      title: '',
+      body: '',
+      public: true,
+    });
+  }
 
 
   return (
